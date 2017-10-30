1bb5fdc4da6c7a7fd6d569bdec7b4b645083773f
xastherion <xastherion@gmail.com>
2017-10-30 14:31:02 +0100

    first

--- youtube01.py
+++ youtube01.py
@@ -2,6 +2,6 @@ import sys
 from tkinter import *
 
 mi_gui = Tk()
-mi_gui.geometry('450x450+800+200')
+mi_gui.geometry('450x450+800+200') # geom. 450x450 tamano y 800 +200 ubicacion
 
 mi_gui.mainloop()
