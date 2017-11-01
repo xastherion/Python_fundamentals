98b4fcc796c29f734154216138efff27b40ca3ba
Carlos Gonzalez <xastherion@users.noreply.github.com>
2017-11-01 09:30:08 +0100

    Update contador.py

--- contador.py
+++ contador.py
@@ -2,4 +2,5 @@ contador = 0
 while (contador < 5):
     print('El contador es :', contador)
     contador = contador + 1
-    print("Fin del programa")
+    print("- Fin del While")
+print("FIN DEL PROGRAMA")
